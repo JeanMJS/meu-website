da0daca7ede5daf754a733e8034249e37ea8a5bd
Jean MJS <jean.marcos.souza@hotmail.com>
2022-05-20 22:41:48 -0300

    test include styles

--- webSite/settings.py
+++ webSite/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +59,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'base_templates'
+            os.path.join(BASE_DIR, 'base_templates')
         ],
             
         'APP_DIRS': True,
